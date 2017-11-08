68093bb3e640bb337b38d46de1f87c8a80b05e94
knightSP <33259705+knightSP@users.noreply.github.com>
2017-11-08 21:59:38 +0530

    First commit

---
+++ cross-plat-win32-x64/resources/app/src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCn_wT7ookNQy1CCMZy2loysZNYRLB4A-M",
+    authDomain: "angular-gallery-f6b38.firebaseapp.com",
+    databaseURL: "https://angular-gallery-f6b38.firebaseio.com",
+    projectId: "angular-gallery-f6b38",
+    storageBucket: "angular-gallery-f6b38.appspot.com",
+    messagingSenderId: "257699566891"
+
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCn_wT7ookNQy1CCMZy2loysZNYRLB4A-M",
+    authDomain: "angular-gallery-f6b38.firebaseapp.com",
+    databaseURL: "https://angular-gallery-f6b38.firebaseio.com",
+    projectId: "angular-gallery-f6b38",
+    storageBucket: "angular-gallery-f6b38.appspot.com",
+    messagingSenderId: "257699566891"
+
+  }
+};
